b74dfcda2b027cf637558ca7474ba475a94d5540
Tyler Brown <tylerjbrown192@gmail.com>
2019-12-07 11:54:42 -0800

    Properly invoke dotenv.config, postgres running & applying migrations

--- w2w-fitness-server/src/index.ts
+++ w2w-fitness-server/src/index.ts
@@ -1,14 +1,14 @@
+import dotenv from 'dotenv';
+import 'reflect-metadata';
+dotenv.config();
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import express from 'express';
-import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 import typeOrmConfig from './server/config';
 
 
 // Configure Server
-dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -17,18 +17,25 @@ app.use(cors());
 // Routes
 // TODO: Break routes out per model once Terminology is feature complete
 app.get('/terminology', (req, res) => {
-    res.send('dj khaled anotha one');
+    res.send('GET terminology');
 });
 
 app.post('/terminology', (req, res) => {
-    res.json('another');
+    res.json('POST terminology');
 });
 
+app.get('/daily-log', (req, res) => {
+    res.send('GET daily log');
+});
+
+app.post('/daily-log', (req, res) => {
+    res.json('POST daily log');
+});
 
 // Init Server
 createConnection(typeOrmConfig).then((connection) => {
-
+    console.log('conn', connection);
     app.listen(process.env.PORT || 3001, () => {
         console.log(`Server is listening on port ${process.env.PORT || 3001}`);
     });
-}).catch((error) => console.log('typeorm createConnection error: ', error));
+}).catch((error) => console.log('typeorm createConnection error: ', error, typeOrmConfig));
